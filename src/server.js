a83d1b6096f3595d0bb0fbf004d7679e9f1f6b2d
yuri <yuri@example.com>
2023-07-15 19:33:13 +0900

    11. スキーマ定義をHackerNews用に拡張してみよう

--- src/server.js
+++ src/server.js
@@ -1,16 +1,33 @@
 const { ApolloServer, gql } = require("apollo-server");
 
+//HackerNewsの投稿データ（本来はデータベースから取得する）
+const links = [
+  {
+    id: "link-0",
+    description: "Udemyで学ぶ",
+    url: "udemy.com",
+  },
+];
+
 //GraphQLのスキーマ（データ構造）の定義
 //!はnullは許容しない
 const typeDefs = gql`
   type Query {
     info: String!
+    feed: [Link]!
+  }
+
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `;
 
 const resolvers = {
   Query: {
     info: () => "Hacknewsクローン",
+    feed: () => links,
   },
 };
 

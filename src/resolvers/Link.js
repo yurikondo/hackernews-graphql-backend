bb9ee94e757f734aa7eb9259303174710aea3bf5
yuri <yuri@example.com>
2023-07-16 16:23:33 +0900

    Linkリゾルバの実装

---
+++ src/resolvers/Link.js
@@ -0,0 +1,12 @@
+//誰によって投稿されたのかのリゾルバ
+function postedBy(parent, args, context) {
+  return context.prisma.link
+    .findUnique({
+      where: { id: parent.id },
+    })
+    .postedBy();
+}
+
+module.exports = {
+  postedBy,
+};

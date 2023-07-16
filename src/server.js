054609f1bf26cdb5944ddeb407039caed9820f5d
yuri <yuri@example.com>
2023-07-16 11:30:27 +0900

    22. contextをリゾルバで利用してデータベースにアクセスしよう

--- src/server.js
+++ src/server.js
@@ -5,31 +5,22 @@ const { PrismaClient, Prisma } = require("@prisma/client");
 
 const prisma = new PrismaClient();
 
-const links = [
-  {
-    id: "link-0",
-    description: "Udemyで学ぶ",
-    url: "udemy.com",
-  },
-];
-
 const resolvers = {
   Query: {
     info: () => "Hacknewsクローン",
-    feed: () => links,
+    feed: async (parent, args, context) => {
+      return context.prisma.link.findMany();
+    },
   },
   Mutation: {
-    post: (parent, args) => {
-      let idCount = links.length;
-
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url,
-      };
-
-      links.push(link);
-      return link;
+    post: (parent, args, context) => {
+      const newLink = context.prisma.link.create({
+        data: {
+          url: args.url,
+          description: args.description,
+        },
+      });
+      return newLink;
     },
   },
 };
